9d85a1e390e78d04f147b955503a3b78293ccfa6
RobertAKARobin <robertgfthomas@gmail.com>
2017-03-08 22:21:25 -0600

    Appends to and displays list

--- public/js/main.js
+++ public/js/main.js
@@ -14,24 +14,21 @@ var ConvoList = (function(){
 	var construct = function(){
 		var list = this;
 		list.ref = ConvoList.base.ref('/convos');
-		list.db = defineDBEvents(list);
+		list.convos = [];
 		list.events = defineDOMEvents(list);
-		list.ref.on('value', list.db.onChange);
-	}
-	var defineDBEvents = function($instance){
-		var list = $instance;
-		var db = {};
-		db.onChange = function(snapshot){
-			console.log(JSON.stringify(snapshot.val()));
-		}
-		return db;
+		list.ref.on('child_added', function(snapshot, previousKey){
+			list.convos.push(snapshot.val());
+			m.redraw();
+		});
 	}
 	var defineDOMEvents = function($instance){
 		var list = $instance;
 		var events = {};
 		events.push = function(event){
+			event.redraw = false;
 			if(event.keyCode == 13){
 				list.ref.push(event.currentTarget.value);
+				event.currentTarget.value = '';
 			}
 		}
 		return events;
@@ -39,9 +36,16 @@ var ConvoList = (function(){
 
 	$instance.view = function(){
 		var list = this;
-		return m('input', {
-			onkeyup: list.events.push
-		});
+		return m('ul', [
+			m('li', [
+				m('input', {
+					onkeyup: list.events.push
+				})
+			]),
+			list.convos.map(function(convo){
+				return m('li', convo)
+			})
+		])
 	}
 
 	return $Class;
