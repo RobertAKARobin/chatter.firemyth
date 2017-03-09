e0aed9b5104ca978e8bf9324f64cdaae7c74fd7f
RobertAKARobin <robertgfthomas@gmail.com>
2017-03-08 21:55:32 -0600

    Basic Firebase service

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,36 @@
 'use strict';
 
+var ConvoList = (function(){
+	var $Class = {};
+	var $instance = {};
+
+ 	$Class.base = firebase.database();
+	$Class.load = function(){
+		var list = Object.create($instance);
+		construct.apply(list, arguments);
+		return list;
+	}
+
+	var construct = function(){
+		var list = this;
+		list.ref = ConvoList.base.ref('/convos');
+		list.db = defineDBEvents(list);
+		list.ref.on('value', list.db.onChange);
+	}
+	var defineDBEvents = function($instance){
+		var list = $instance;
+		var db = {};
+		db.onChange = function(snapshot){
+			console.log(snapshot.val());
+		}
+		return db;
+	}
+
+	return $Class;
+})();
+
 document.addEventListener('DOMContentLoaded', function(){
-	var root = firebase.database().ref('/');
-	root.on('value', function(snapshot){
-		console.log(snapshot.numChildren());
-	});
+	var convoList = ConvoList.load();
 
 	m.mount(document.getElementById('app'), {
 		view: function(){
